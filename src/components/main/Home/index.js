009571cdfe35bca10738b9a518b68bc727aa0f67
Sharav2022 <144260690+Sharav2022@users.noreply.github.com>
2023-09-09 02:23:55 +0530

    Delete src directory

--- src/components/main/Home/index.js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const ServicesSection = dynamic(() => import("./components/ServicesSection"));
-const MainCarouselSection = dynamic(() =>
-  import("./components/MainCarouselSection")
-);
-const WhyusSection = dynamic(() => import("./components/WhyusSection"));
-
-export { ServicesSection, WhyusSection, MainCarouselSection };
